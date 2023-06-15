33b4d6e620bafa166b902fef0b852811e066705b
Rohit <rohitsaste45@gmail.com>
2023-06-15 22:22:51 +0530

    basic

---
+++ my-react-app/src/App.js
@@ -0,0 +1,61 @@
+import { useRef, useState } from "react";
+
+function App() {
+  return (
+    <>
+      <h1>My Project</h1>
+      <ListDemo />
+    </>
+  );
+}
+
+function ListDemo() {
+  let inputRef = useRef();
+  let [list, setList] = useState(["delhi"]);
+
+  let addItemAction = () => {
+    let inputValue = inputRef.current.value;
+    let newList = [inputValue, ...list];
+    setList(newList);
+
+    inputRef.current.value = "";
+  };
+
+  return (
+    <>
+      <div
+        className="row justify-content-center align-items-center"
+        style={{ height: "100vh" }}
+      >
+        <div className="col-sm-12 col-md-6">
+          <h1>Registraton App</h1>
+          <input
+            className="form-control"
+            type="text"
+            id="id1"
+            ref={inputRef}
+            placeholder="Enter user input..."
+          />
+          <input
+            className="form-control"
+            type="text"
+            id="id1"
+            ref={inputRef}
+            placeholder="Enter Password..."
+          />
+          <input
+            className="form-control"
+            type="text"
+            id="id1"
+            ref={inputRef}
+            placeholder="Enter Email..."
+          />
+          <input type="button" value="Login" onClick={addItemAction} />
+        </div>
+      </div>
+    </>
+  );
+}
+
+
+export default App;

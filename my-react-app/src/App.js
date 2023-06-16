18293719908cc0e0b441068b66f98a2490635367
Rohit <rohitsaste45@gmail.com>
2023-06-16 11:51:05 +0530

    b21

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,14 +1,29 @@
-import { useRef, useState } from "react";
+import { BrowserRouter, Route,Link, Routes } from "react-router-dom";
 import MyTodo from "./Pages/MyTodo";
 import MyRegistration from "./Pages/MyRegistration";
+
 
 function App() {
   return (
     <>
-      <h1>My Todo</h1>
-      <MyTodo />
-      <MyRegistration/>
+      <BrowserRouter>
+        <Link to={"/home"}>Home</Link>
+        <Link to={"/todo"}>Todo</Link>
+        <Link to={"/registration"}>Registration</Link>
+        <Routes>
+          <Route path="/" element={<h1>Hello</h1>} />
+          <Route path="/home" element={<h1>Home</h1>} />
+          <Route path="/todo" element={<MyTodo />} />
+          <Route path="/registration" element={<MyRegistration />} />
+          <Route path="*" element={<h1>Page Not Found</h1>} />
+
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
 export default App;
+
+// <h1>My Todo</h1>
+//       <MyTodo />
+//       <MyRegistration/>
--- my-react-app/src/Pages/MyRegistration.js
+++ my-react-app/src/Pages/MyRegistration.js
@@ -1,82 +1,85 @@
 import { useState } from "react";
 
+function MyRegistration() {
+  let [user, setUser] = useState({
+    username: "",
+    password: "",
+    email: "",
+    mobile: "",
+  });
 
- function MyRegistration() {
+  let handlerUsernameAction = (e) => {
+    let newuser = { ...user, username: e.target.value };
+    setUser(newuser);
+  };
 
-    let [user, setUser] = useState({
-        username: " ",
-        password: " ",
-        email: " ",
-        mobile: " ",
-    });
+  let handlerPasswordAction = (e) => {
+    let newuser = { ...user, password: e.target.value };
+    setUser(newuser);
+  };
 
-    let handlerUsernameAction = (e) =>{
-        let newuser = {...user,username:e.target.value}
-        setUser(newuser)
-    }
-    let handlerPasswordAction = (e) =>{
-        let newuser = {...user,password:e.target.value}
-        setUser(newuser)
-    }
-    let handlerEmailAction = (e) =>{
-        let newuser = {...user,email:e.target.value}
-        setUser(newuser)
-    }
-    let handlerMobileAction = (e) =>{
-        let newuser = {...user,mobile:e.target.value}
-        setUser(newuser)
-    }
-    let registrationAction = async ( )=>{
-        let url = `http://localhost:4000/adduser?username=${user.username}&password=${user.password}&email=${user.email}&mobile=${user.mobile}`;
-        await fetch(url);
-        let newuser = {
-            username: "",
-            password: "",
-            email: "",
-            mobile: "",
-          };
-          setUser(newuser);
+  let handlerEmailAction = (e) => {
+    let newuser = { ...user, email: e.target.value };
+    setUser(newuser);
+  };
+
+  let handlerMobileAction = (e) => {
+    let newuser = { ...user, mobile: e.target.value };
+    setUser(newuser);
+  };
+
+  let registerAction = async () => {
+    // BACKEND
+    let url = `http://localhost:4000/adduser?username=${user.username}&password=${user.password}&email=${user.email}&mobile=${user.mobile}`;
+    await fetch(url);
+
+    let newuser = {
+      username: "",
+      password: "",
+      email: "",
+      mobile: "",
     };
+    setUser(newuser);
+  };
 
-    return (
-        <>
-            <input
-                className="form-control"
-                type="text"
-                placeholder="Enter Username..."
-                value={user.username}
-                onChange={handlerUsernameAction} />
-
-            <input
-                className="form-control"
-                type="password"
-                placeholder="Enter Password..."
-                value={user.password}
-                onChange={handlerPasswordAction} />
-
-            <input
-                className="form-control"
-                type="text"
-                placeholder="Enter Email..."
-                value={user.email}
-                onChange={handlerEmailAction} />
-
-            <input
-                className="form-control"
-                type="text"
-                placeholder="Enter MobileNumbar..."
-                value={user.mobile}
-                onChange={handlerMobileAction} />
-
-            <input 
-                type="button" 
-                value="register"
-                className="w-100"
-                onClick={registrationAction}
-                />
-
-        </>
-    );
+  return (
+    <>
+      <input
+        type="text"
+        className="form-control"
+        placeholder="Enter username"
+        value={user.username}
+        onChange={handlerUsernameAction}
+      />
+      <input
+        type="password"
+        className="form-control"
+        placeholder="Enter password"
+        value={user.password}
+        onChange={handlerPasswordAction}
+      />
+      <input
+        type="text"
+        className="form-control"
+        placeholder="Enter Email"
+        value={user.email}
+        onChange={handlerEmailAction}
+      />
+      <input
+        type="text"
+        className="form-control"
+        placeholder="Enter mobile"
+        value={user.mobile}
+        onChange={handlerMobileAction}
+      />
+      <input
+        type="button"
+        value="Register"
+        className="w-100"
+        onClick={registerAction}
+      />
+    </>
+  );
 }
 
 export default MyRegistration;
